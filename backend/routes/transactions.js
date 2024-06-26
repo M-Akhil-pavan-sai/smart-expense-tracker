441495dc5f68a73243902e37234345e63fc8600a
M-Akhil-Pavan-Sai <9701209751maps@gmail.com>
2024-06-26 13:33:35 -0400

    Backend: Updated Routes

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -8,10 +8,10 @@ router.get("/",(req,res)=>{
 })
 
 router.post("/add-income",addIncome)
-    .get("/incomes",getIncomes)
+    .get("/get-incomes",getIncomes)
     .delete("/delete-income/:id",deleteIncome)
     .post("/add-expense",addExpense)
-    .get("/expenses",getExpenses)
+    .get("/get-expenses",getExpenses)
     .delete("/delete-expense/:id",deleteExpense)
 
 module.exports = router
